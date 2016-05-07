0762b90d4e0f78ca608f326df5b67e0dbeddcb7e
merlin <yuyankai945@hotmail.com>
2016-05-07 15:47:38 +0800

    first check and test Xcode of c edit

---
+++ C_First/C_First/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  C_First
+//
+//  Created by merlin on 16/5/7.
+//  Copyright © 2016年 merlin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    printf("Hello, World!\n");
+    return 0;
+}
